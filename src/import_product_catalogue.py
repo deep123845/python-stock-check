6e214f1cfe9927ca2e13fbe25961cf4219035c50
Deep123845 <deep123845@gmail.com>
2025-11-11 18:43:06 -0500

    catalogue inputs now have any quotes stripped from them

--- src/import_product_catalogue.py
+++ src/import_product_catalogue.py
@@ -9,11 +9,16 @@ class ProudctListing:
 	wholesale_case_price: float
 	units_per_case: int
 
+def extract_from_quotes(input: str):
+	parts = input.split('"')
+
+	return parts[1]
+
 def create_product_listing(row) -> ProudctListing:
-	lcbo_number = row['SKU #']
-	upc = row['UPC']
+	lcbo_number = extract_from_quotes(row['SKU #'])
+	upc = extract_from_quotes(row['UPC'])
 	supplier = row['SUPPLYING SOURCE']
-	wholesale_case_price = row['WHOLESALE PRICE PER CASE']
+	wholesale_case_price = float(extract_from_quotes(row['WHOLESALE PRICE PER CASE']))
 	units_per_case = row['SELLING UNITS PER CASE']
 
 	return ProudctListing(lcbo_number, upc, supplier, wholesale_case_price, units_per_case)
