eb0332c76f58c464c1c211dcc5ed4471897a83cd
Deep123845 <deep123845@gmail.com>
2025-11-11 18:49:13 -0500

    fixed typo in product listing class name

--- src/import_product_catalogue.py
+++ src/import_product_catalogue.py
@@ -2,7 +2,7 @@ import csv
 from dataclasses import dataclass
 
 @dataclass
-class ProudctListing:
+class ProudcutListing:
 	lcbo_number: str
 	upc: str
 	supplier: str
@@ -14,14 +14,14 @@ def extract_from_quotes(input: str):
 
 	return parts[1]
 
-def create_product_listing(row) -> ProudctListing:
+def create_product_listing(row) -> ProudcutListing:
 	lcbo_number = extract_from_quotes(row['SKU #'])
 	upc = extract_from_quotes(row['UPC'])
 	supplier = row['SUPPLYING SOURCE']
 	wholesale_case_price = float(extract_from_quotes(row['WHOLESALE PRICE PER CASE']))
 	units_per_case = row['SELLING UNITS PER CASE']
 
-	return ProudctListing(lcbo_number, upc, supplier, wholesale_case_price, units_per_case)
+	return ProudcutListing(lcbo_number, upc, supplier, wholesale_case_price, units_per_case)
 
 product_catalogue = []
 
