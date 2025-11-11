6a3501a5833a63e8442e1263e440acb3c2efbd24
Deep123845 <deep123845@gmail.com>
2025-11-11 18:52:14 -0500

    created function to create a list of product listings + actually fixed typo

--- src/import_product_catalogue.py
+++ src/import_product_catalogue.py
@@ -2,7 +2,7 @@ import csv
 from dataclasses import dataclass
 
 @dataclass
-class ProudcutListing:
+class ProuductListing:
 	lcbo_number: str
 	upc: str
 	supplier: str
@@ -14,22 +14,27 @@ def extract_from_quotes(input: str):
 
 	return parts[1]
 
-def create_product_listing(row) -> ProudcutListing:
+def create_product_listing(row) -> ProuductListing:
 	lcbo_number = extract_from_quotes(row['SKU #'])
 	upc = extract_from_quotes(row['UPC'])
 	supplier = row['SUPPLYING SOURCE']
 	wholesale_case_price = float(extract_from_quotes(row['WHOLESALE PRICE PER CASE']))
 	units_per_case = row['SELLING UNITS PER CASE']
 
-	return ProudcutListing(lcbo_number, upc, supplier, wholesale_case_price, units_per_case)
+	return ProuductListing(lcbo_number, upc, supplier, wholesale_case_price, units_per_case)
 
-product_catalogue = []
-
-with open('CatalogDownload_Convenience.csv') as catalogue:
-	reader = csv.DictReader(catalogue, dialect='excel')
+def create_product_catalogue(reader) -> ProuductListing:
+	product_catalogue = []
+	
 	for row in reader:
 		listing = create_product_listing(row)
 		product_catalogue.append(listing)
+	
+	return product_catalogue
+
+with open('CatalogDownload_Convenience.csv') as catalogue:
+	reader = csv.DictReader(catalogue, dialect='excel')
+	product_catalogue = create_product_catalogue(reader)
 
 for listing in product_catalogue:
 	print(listing)
